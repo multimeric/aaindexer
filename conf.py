dbf5a1c9f519c41b5f8139261549de069452fdba
Michael Milton <michael.r.milton@gmail.com>
2022-05-21 00:11:59 +1000

    Clean up conf.py

--- conf.py
+++ conf.py
@@ -1,44 +1,15 @@
-# -- Project information -----------------------------------------------------
-
 project = 'aaindexer'
 copyright = '2022, Michael Milton'
 author = 'Michael Milton'
-
-# The full version, including alpha/beta/rc tags
 release = '0.1.0'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinxcontrib.restbuilder',
     'sphinx.ext.autodoc',
     'sphinx_click'
 ]
-
 autodoc_typehints = "description"
 autodoc_class_signature = "separated"
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
